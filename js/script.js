d549927d83fa13e4d390adab24734b57a4a5d1d3
Dmitriy <kehayner@gmail.com>
2021-12-14 22:46:04 +0500

    modified cycles and conditions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let numberOfFolms = prompt('How many films you whathed?', '');
+let numberOfFolms = +prompt('How many films you whathed?', '');
 let personalMovieDB = {
   count: numberOfFolms,
   movies: {},
@@ -9,10 +9,29 @@ let personalMovieDB = {
   private: false
 };
 
-let nameFilm = prompt('1st of the last films?', '');
-let rateFilm = prompt('Your raiting of this film?', '');
-let nameFilm2 = prompt('1st of the last films?', '');
-let rateFilm2 = prompt('Your raiting of this film?', '');
+outer:
+for(let i = 0; i < 2; i++) {
+  let nameFilm = prompt('One of the last films?', '');
 
-personalMovieDB.movies[nameFilm] = rateFilm;
-personalMovieDB.movies[nameFilm2] = rateFilm2;
+  while(nameFilm){
+    let rateFilm = prompt('Your raiting of this film?', '');
+    personalMovieDB.movies[nameFilm] = rateFilm;
+    continue outer;
+  }
+
+  if(!nameFilm) {
+    i--;
+    continue outer;
+  }
+  
+}
+
+if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert('You are classic film viewer');
+} else if(personalMovieDB.count < 10) {
+  alert('Few film watched');
+} else {
+  alert('You are movie fan!');
+}
+
+
